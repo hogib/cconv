eb81f59bcb2691f63a39807c9c3e076e9771b3b7
oguzb <hoguzbolat@gmail.com>
2026-05-24 13:03:01 +0300

    implementation tests branched off

--- src/imp_tests/args_loop.h
+++
@@ -1,7 +0,0 @@
-#ifndef ARGS_LOOP_H
-#define ARGS_LOOP_H
-
-#include <unistd.h>
-
-int get_args(int argc, char *argv[]);
-#endif
--- src/imp_tests/getargs_loop.h
+++
@@ -1,8 +0,0 @@
-#ifndef GETARGS_LOOP_H
-#define GETARGS_LOOP_H
-
-#include <unistd.h>
-
-int get_args(int argc, char *argv[]);
-
-#endif
